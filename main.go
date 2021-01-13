8e077246e37be6ae495c334e998fea105f57400e
bsenel <beratcansenel@gmail.com>
2021-01-13 10:22:41 +0100

    Clean the code a bit

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 
 	jaegerIndex := flag.String("index", "jaeger-span-2021-01-06", "index name")
 	flag.Parse()
-	body := strings.NewReader(`{ "from": 0, "size": 500, "sort": [{"startTime": {"order": "asc"}}], "query": { "bool": { "must": [{"match": { "operationName": "cachecash.com/Client/GetObject" }}], "filter": {"range": { "duration": { "gte": 0, "lte": 50000000 }}}}}}`)
+	body := strings.NewReader(`{ "from": 0, "size": 1500, "sort": [{"startTime": {"order": "asc"}}], "query": { "bool": { "must": [{"match": { "operationName": "cachecash.com/Client/GetObject" }}], "filter": {"range": { "duration": { "gte": 0, "lte": 50000000 }}}}}}`)
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%v/_search?pretty", config.ESAPI, string(*jaegerIndex)), body)
 	if err != nil {
 		// handle err
@@ -97,6 +97,12 @@ func main() {
 		}
 		traceArr := traceDat["data"].([]interface{})
 		trace := traceArr[0].(map[string]interface{})
+
+		// CATEGORIZE
+		headers := false
+		if i == 0 {
+			headers = true
+		}
 		var columnNumber = 1
 		var columnDecryptNumber = 1
 		var columnChunkNumber = 1
@@ -108,7 +114,7 @@ func main() {
 		groupedChunkList := map[string]*handleOperations{}
 		handleBundleList := []string{}
 		handleBundleGroup := []int{}
-		var requestBundleCounter int = -1
+		requestBundleCounter := -1
 		for _, traceSpanRow := range trace["spans"].([]interface{}) {
 			spanRow := traceSpanRow.(map[string]interface{})
 			operationName := spanRow["operationName"].(string)
@@ -122,7 +128,7 @@ func main() {
 			columnOrderedChunk, _ := excelize.ColumnNumberToName(columnOrderedChunkNumber)
 
 			if operationName == "cachecash.com/Client/GetObject" {
-				if i == 0 {
+				if headers {
 					title := "Transactions"
 					setHeader(f, "transaction", title, column, i+1, 0)
 					setHeader(f, "transaction", title, columnDecrypt, len(hitsChild)+4+i+1, 0)
@@ -148,7 +154,7 @@ func main() {
 				columnChunk, _ := excelize.ColumnNumberToName(columnChunkNumber)
 				columnOrderedChunk, _ = excelize.ColumnNumberToName(columnOrderedChunkNumber)
 
-				if i == 0 {
+				if headers {
 					title := "Duration (s)"
 					setHeader(f, "", title, column, i+2, 0)
 					setHeader(f, "", title, columnDecrypt, len(hitsChild)+4+i+2, 0)
@@ -165,7 +171,7 @@ func main() {
 				columnOrderedChunkNumber++
 			} else if operationName == "cachecash.com/Client/requestBundle" {
 				if !ttfbCalculated {
-					if i == 0 {
+					if headers {
 						title := "~Publisher TTFB (ms)"
 						setHeader(f, "", title, column, i+2, 0)
 					}
@@ -174,7 +180,6 @@ func main() {
 				}
 				handleBundleGroup = append(handleBundleGroup, 0)
 				requestBundleCounter++
-				//chunkCounter = 0
 			} else if operationName == "cachecash.com/Client/HandleBundle" {
 				if !ttfbCalculated {
 					ttfbStart = spanRow["startTime"].(float64)
@@ -190,7 +195,7 @@ func main() {
 						groupedChunkList[refRow["spanID"].(string)].Puzzle = duration.Milliseconds()
 
 						if handleBundleList[0] == refRow["spanID"].(string) && !ttfbCalculated {
-							if i == 0 {
+							if headers {
 								title := "Caches TTFB (ms)"
 								setHeader(f, "", title, column, i+2, 0)
 							}
@@ -206,138 +211,76 @@ func main() {
 				references := spanRow["references"].([]interface{})
 				for _, refRowInt := range references {
 					refRow := refRowInt.(map[string]interface{})
-
 					if refRow["refType"].(string) == "CHILD_OF" {
 						groupedChunkList[refRow["spanID"].(string)].Chunk = append(groupedChunkList[refRow["spanID"].(string)].Chunk, duration.Milliseconds())
 					}
 				}
-
-				/*if len(chunkList) < 4 {
-					chunkList = append(chunkList, duration.Milliseconds())
-				}
-				if len(chunkList) == 4 {
-					sort.Slice(chunkList, func(k, l int) bool { return chunkList[k] < chunkList[l] })
-
-					columnOrderedChunkNumber = columnOrderedChunkNumber - 3
-					for _, chunkRow := range chunkList {
-						columnOrderedChunk, _ = excelize.ColumnNumberToName(columnOrderedChunkNumber)
-						f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnOrderedChunk, (len(hitsChild)+4)*3+i+3), chunkRow)
-						columnOrderedChunkNumber++
-					}
-					chunkList = []int64{}
-				} else {
-					columnOrderedChunkNumber++
-				}*/
-
-				/*if i == 0 {
-					if chunkCounter == 12 {
-						title := "Last"
-						setHeader(f, "chunk", title, columnChunk, (len(hitsChild)+4)*2+i+1, chunkCounter-1)
-					} else {
-						title := fmt.Sprintf("%d.", chunkCounter)
-						setHeader(f, "", title, columnChunk, (len(hitsChild)+4)*2+i+1, 0)
-					}
-				}*/
-
 			}
-
-			/*if j == len(trace["spans"].([]interface{}))-1 && len(chunkList) != 0 {
-				sort.Slice(chunkList, func(k, l int) bool { return chunkList[k] < chunkList[l] })
-				columnOrderedChunkNumber = columnOrderedChunkNumber - len(chunkList)
-				for _, chunkRow := range chunkList {
-					columnOrderedChunk, _ = excelize.ColumnNumberToName(columnOrderedChunkNumber)
-					f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnOrderedChunk, (len(hitsChild)+4)*3+i+3), chunkRow)
-					columnOrderedChunkNumber++
-				}
-				chunkList = []int64{}
-				if chunkCounter != 12 {
-					title := "Last"
-					setHeader(f, "chunk", title, columnChunk, (len(hitsChild)+4)*3+i+1, chunkCounter -1)
-				}
-				chunkCounter = 0
-			}*/
 		}
-		var chunkCounter int = 1
-		var sortedChunkCounter int = 1
 
+		chunkCounter := 1
 		for hn, handleBundle := range handleBundleList {
-
-			puzzleDecryption := groupedChunkList[handleBundle].Puzzle
-
+			// PUZZLE
+			decryptDuration := groupedChunkList[handleBundle].Puzzle
+			// FIRST SECTION
 			column, _ := excelize.ColumnNumberToName(columnNumber)
-			f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", column, i+3), puzzleDecryption)
+			f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", column, i+3), decryptDuration)
 			if decyrptCounter < 3 {
-				if i == 0 {
+				if headers {
 					title := fmt.Sprintf("%d.", decyrptCounter+1)
 					setHeader(f, "", title, column, i+2, 0)
 				}
 				columnNumber++
-			} else if i == 0 {
+			} else if headers {
 				title := "Last"
 				setHeader(f, "", title, column, i+2, 0)
 				title = "Decrypt Puzzle Duration (ms)"
 				setHeader(f, "puzzle", title, column, i+1, 3)
 			}
-
+			// PUZZLE DECRYPTION LIST
 			columnDecrypt, _ := excelize.ColumnNumberToName(columnDecryptNumber)
-			f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnDecrypt, len(hitsChild)+4+i+3), puzzleDecryption)
-			if i == 0 {
+			f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnDecrypt, len(hitsChild)+4+i+3), decryptDuration)
+			if headers {
 				title := fmt.Sprintf("%d. Decrypt", decyrptCounter+1)
 				setHeader(f, "", title, columnDecrypt, len(hitsChild)+4+i+2, 0)
 			}
 			decyrptCounter++
 			columnDecryptNumber++
 
+			// CHUNKS
 			groupedChunks := groupedChunkList[handleBundle].Chunk
-			for key, chunkDuration := range groupedChunks {
-				columnChunk, _ := excelize.ColumnNumberToName(columnChunkNumber)
-				f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnChunk, (len(hitsChild)+4)*2+i+3), chunkDuration)
-				if i == 0 {
-					title := fmt.Sprintf("%d.", chunkCounter)
-					setHeader(f, "", title, columnChunk, (len(hitsChild)+4)*2+i+2, 0)
-					if checkBundleChange(handleBundleGroup, hn+1) && key == len(groupedChunks)-1 {
-						title = "Chunks in Bundle"
-						setHeader(f, "bundle-chunks", title, columnChunk, (len(hitsChild)+4)*2+i, chunkCounter-1)
-						title = fmt.Sprintf("%d. Handle Bundle", hn+1)
-						setHeader(f, "handle-chunks", title, columnChunk, (len(hitsChild)+4)*2+i+1, len(groupedChunks)-1)
-						chunkCounter = 0
-					} else if key == len(groupedChunks)-1 {
-						title = fmt.Sprintf("%d. Handle Bundle", hn+1)
-						setHeader(f, "handle-chunks", title, columnChunk, (len(hitsChild)+4)*2+i+1, len(groupedChunks)-1)
-					}
-				}
-				chunkCounter++
-
-				columnChunkNumber++
-			}
+			handleChunkGroups(f, groupedChunks, handleBundleGroup, (len(hitsChild)+4)*3+i, columnChunkNumber, hn, chunkCounter, headers)
 			sortChunkGroup := groupedChunks
 			sort.Slice(sortChunkGroup, func(k, l int) bool { return sortChunkGroup[k] < sortChunkGroup[l] })
-
-			for key, chunkDuration := range sortChunkGroup {
-				columnOrderedChunk, _ := excelize.ColumnNumberToName(columnOrderedChunkNumber)
-				f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnOrderedChunk, (len(hitsChild)+4)*3+i+3), chunkDuration)
-				if i == 0 {
-					title := fmt.Sprintf("%d.", sortedChunkCounter)
-					setHeader(f, "", title, columnOrderedChunk, (len(hitsChild)+4)*3+i+2, 0)
-					if checkBundleChange(handleBundleGroup, hn+1) && key == len(sortChunkGroup)-1 {
-						title = "Chunks in Bundle"
-						setHeader(f, "bundle-chunks", title, columnOrderedChunk, (len(hitsChild)+4)*3+i, sortedChunkCounter-1)
-						title = fmt.Sprintf("%d. Handle Bundle", hn+1)
-						setHeader(f, "handle-chunks", title, columnOrderedChunk, (len(hitsChild)+4)*3+i+1, len(sortChunkGroup)-1)
-						sortedChunkCounter = 0
-					} else if key == len(sortChunkGroup)-1 {
-						title = fmt.Sprintf("%d. Handle Bundle", hn+1)
-						setHeader(f, "handle-chunks", title, columnOrderedChunk, (len(hitsChild)+4)*3+i+1, len(sortChunkGroup)-1)
-					}
-				}
-
-				sortedChunkCounter++
-				columnOrderedChunkNumber++
-			}
+			columnChunkNumber, chunkCounter = handleChunkGroups(f, sortChunkGroup, handleBundleGroup, (len(hitsChild)+4)*2+i, columnChunkNumber, hn, chunkCounter, headers)
 		}
 	}
 
 	f.SaveAs(fmt.Sprintf("./%v.xlsx", string(*jaegerIndex)))
+}
+
+func handleChunkGroups(f *excelize.File, groupedChunks []int64, handleBundleGroup []int, row, column, handleNumber, chunkCounter int, headers bool) (int, int) {
+	for key, chunkDuration := range groupedChunks {
+		columnChunk, _ := excelize.ColumnNumberToName(column)
+		f.SetCellValue("Sheet1", fmt.Sprintf("%s%d", columnChunk, row+3), chunkDuration)
+		if headers {
+			title := fmt.Sprintf("%d.", chunkCounter)
+			setHeader(f, "", title, columnChunk, row+2, 0)
+			if checkBundleChange(handleBundleGroup, handleNumber+1) && key == len(groupedChunks)-1 {
+				title = "Chunks in Bundle"
+				setHeader(f, "bundle-chunks", title, columnChunk, row, chunkCounter-1)
+				title = fmt.Sprintf("%d. Handle Bundle", handleNumber+1)
+				setHeader(f, "handle-chunks", title, columnChunk, row+1, len(groupedChunks)-1)
+				chunkCounter = 0
+			} else if key == len(groupedChunks)-1 {
+				title = fmt.Sprintf("%d. Handle Bundle", handleNumber+1)
+				setHeader(f, "handle-chunks", title, columnChunk, row+1, len(groupedChunks)-1)
+			}
+		}
+		column++
+		chunkCounter++
+	}
+	return column, chunkCounter
 }
 
 func checkBundleChange(handleBundleGroup []int, handleBundleRow int) bool {
